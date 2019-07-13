90f95c28f99344c8fe18e30d8213959df619265b
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-07-13 08:53:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "#"
+//var prefix = "#"
 const fs = require("fs"); 
 const moment = require("moment");  
 const pretty = require("pretty-ms");
 const ms = require("ms");
 const wait = require('util').promisify(setTimeout);
 client.on('ready', () => {
-var x = client.channels.get("581855832002789377");
+var x = client.channels.get("599118836679049240");
 if (x) x.join();
 });
 
 client.on("ready", () => {
-client.user.setStatus('online');
-client.user.setGame("Voltes System", "https://www.twitch.tv/idk");
+client.user.setStatus('offline');
+client.user.setGame("~", "https://www.twitch.tv/idk");
   console.log(`Logged in as ${client.user.tag}!`);
   //client.user.setActivity("Voltes System",{type: 'LISTENING'});
 });
 
 client.on("message", message => {
-if(message.content.startsWith(prefix + "avatar")){
-if(!message.guild.member(message.author).hasPermission("8")) return message.reply("You don't have Permission").then(msg => msg.delete(1000));
+//if(message.content.startsWith(prefix + "avatar")){
+if(message.content.startsWith("av")){
+//if(!message.guild.member(message.author).hasPermission("8")) return message.reply("You don't have Permission").then(msg => msg.delete(1000));
 if(message.author.bot || message.channel.type == "dm") return;
 var args = message.content.split(" ")[1];
 var avt = args || message.author.id;
@@ -31,7 +32,7 @@ let avtEmbed = new Discord.RichEmbed()
 .setColor("#36393e")
 .setAuthor(`${avt.username}'s Avatar`, message.author.avatarURL)
 .setImage(avt.avatarURL)
-.setFooter('voltes', message.guild.iconURL)
+.setFooter(message.guild.iconURL, '0001.')
 .setTimestamp()
 message.channel.send(avtEmbed);
 })
